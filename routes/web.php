40078bff3e5a22bf1ef2ff8bae148ec3626104c0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-19 21:56:24 +0200

    add routes for recipes, add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,14 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('recipes', RecipeController::class)->name('recipes.index');
+Route::get('recipes/create', RecipeController::class)->name('recipes.create');
+Route::post('recipes', RecipeController::class)->name('recipes.store');
+Route::get('recipes/{recipe}', RecipeController::class)->name('recipes.show');
+Route::get('recipes/{recipe}/edit', RecipeController::class)->name('recipes.edit');
+Route::patch('recipes/{recipe}', RecipeController::class)->name('recipes.update');
+Route::delete('recipes/{recipe}', RecipeController::class)->name('recipes.destroy');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
