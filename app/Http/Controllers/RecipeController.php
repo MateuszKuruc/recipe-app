2cbf4fe170a8a76132513ff558858d457a0e2648
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-22 16:52:07 +0200

    add tags loading with recipes db call

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipe;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class RecipeController extends Controller
 {
-    //
+    public function index()
+    {
+        // need to paginate later on to improve efficiency
+        $recipes = Recipe::with('tags')->get();
+        return inertia::render('recipes/Index', [
+            'recipes' => $recipes,
+        ]);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show(Recipe $recipe)
+    {
+
+    }
+
+    public function edit(Recipe $recipe)
+    {
+
+    }
+
+    public function update(Request $request, Recipe $recipe)
+    {
+
+    }
+
+    public function destroy(Recipe $recipe)
+    {
+
+    }
 }
