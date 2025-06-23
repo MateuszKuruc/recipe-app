8fcad37d7c0186e1e300f2fffeaa1d3d48157ef3
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-24 01:11:02 +0200

    add route for single category

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('przepisy/{recipe}', [RecipeController::class, 'show'])->name('recipe
 //Route::delete('recipes/{recipe}', RecipeController::class)->name('recipes.destroy');
 
 Route::get('kategorie', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('kategorie/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
