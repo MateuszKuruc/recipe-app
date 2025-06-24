2fa7ce0a414b0a86ff475668304b4f07f80ea192
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-24 14:38:08 +0200

    add route for recipe store

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
 
 Route::get('przepisy', [RecipeController::class, 'index'])->name('recipes.index');
 Route::get('przepisy/dodaj', [RecipeController::class, 'create'])->name('recipes.create');
-//Route::post('recipes', RecipeController::class)->name('recipes.store');
+Route::post('przepisy', [RecipeController::class, 'store'])->name('recipes.store');
 Route::get('przepisy/{recipe}', [RecipeController::class, 'show'])->name('recipes.show');
 //Route::get('recipes/{recipe}/edit', RecipeController::class)->name('recipes.edit');
 //Route::patch('recipes/{recipe}', RecipeController::class)->name('recipes.update');
