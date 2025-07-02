2b3681f108e308239cf096203a9b70f8a1edb9e2
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-03 01:17:15 +0200

    change user model to handle favorite recipes

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,7 +53,7 @@ class User extends Authenticatable
         return $this->hasMany(Recipe::class);
     }
 
-    public function favoriteRecipes(): HasMany
+    public function favoriteRecipes(): BelongsToMany
     {
         return $this->belongsToMany(Recipe::class, 'favorites');
     }
