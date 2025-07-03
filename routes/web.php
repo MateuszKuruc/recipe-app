d623dfd9cb1c619f0c1fd26c65461b005ce23670
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-03 18:07:54 +0200

    add routes for handling showing favorite recipes and showing random favorite recipe

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,14 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->group(function () {
     Route::get('przepisy/dodaj', [RecipeController::class, 'create'])->name('recipes.create');
     Route::post('przepisy', [RecipeController::class, 'store'])->name('recipes.store');
-    Route::post('przepisy/{recipe}/ulubione', [RecipeController::class, 'favorite'])->name('recipes.favorite');
-    Route::delete('przepisy/{recipe}/ulubione', [RecipeController::class, 'unfavorite'])->name('recipes.unfavorite');
     Route::get('przepisy/{recipe}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
     Route::match(['put', 'post'], 'przepisy/{recipe}', [RecipeController::class, 'update'])->name('recipes.update');
-    Route::delete('recipes/{recipe}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
+    Route::delete('przepisy/{recipe}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
+
+    Route::get('przepisy/ulubione', [RecipeController::class, 'showFavorites'])->name('recipes.favorites');
+    Route::get('przepisy/ulubione/losowy', [RecipeController::class, 'showRandomFavorite'])->name('recipes.randomFavorite');
+    Route::post('przepisy/{recipe}/ulubione', [RecipeController::class, 'favorite'])->name('recipes.favorite');
+    Route::delete('przepisy/{recipe}/ulubione', [RecipeController::class, 'unfavorite'])->name('recipes.unfavorite');
 });
 
 Route::get('przepisy', [RecipeController::class, 'index'])->name('recipes.index');
