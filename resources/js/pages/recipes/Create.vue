83cf34db04b8cc468f9e622e7f3a9acdff3b8990
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-03 21:41:53 +0200

    add breadcrumbs to create form component

--- resources/js/pages/recipes/Create.vue
+++ resources/js/pages/recipes/Create.vue
@@ -4,20 +4,26 @@ import AuthBase from '@/layouts/AuthLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import AppLayout from '@/layouts/AppLayout.vue';
 import RecipeForm from '@/components/RecipeForm.vue';
+import { type BreadcrumbItem } from '@/types';
 
 defineProps({
     categories: Array,
     tags: Array,
 })
 
-</script>
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Nowy przepis',
+        href: '/kategorie',
+    },
+];
 
+</script>
 <template>
-    <AppLayout>
+    <AppLayout :breadcrumbs="breadcrumbs">
         <AuthBase title="Dodaj nowy przepis" description="Uzupełnij wymagane pola i zapisz swój przepis!">
             <Head title="Nowy przepis" />
                 <RecipeForm :categories="categories" :tags="tags" />
-
         </AuthBase>
     </AppLayout>
 </template>
