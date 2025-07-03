a1b6a15bdb2a82c19e2da2aca7046debdf115e80
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-04 01:38:50 +0200

    change recipe card block to correctly display on hp

--- resources/js/components/RecipeCardBlock.vue
+++ resources/js/components/RecipeCardBlock.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
+import RecipeBadge from '@/components/RecipeBadge.vue';
 import { Link } from '@inertiajs/vue3';
 
-defineProps<{
-    image: string;
-    title: string;
-    href: string;
-}>();
+defineProps({
+    image: String,
+    title: String,
+    href: String,
+    tags: Array,
+});
 </script>
 
 <template>
-    <div class="overflow-hidden rounded-lg">
-        <Link :href="href">
-            <img :src="image" :alt="title" class="h-48 w-full object-cover" />
+    <div class="flex h-full flex-col rounded-xl shadow bg-orange-50">
+        <!-- Image -->
+        <Link :href="href" class="block">
+            <img :src="image" :alt="title" class="aspect-[3/4] w-full object-cover rounded-xl" />
         </Link>
-        <div class="mt-4">
+
+        <!-- Content -->
+        <div class="flex flex-1 flex-col justify-between px-4 py-3">
             <Link :href="href">
-                <h3 class="text-lg leading-snug font-bold text-orange-500">
+                <h3 class="text-lg leading-snug font-bold text-orange-500 break-words">
                     {{ title }}
                 </h3>
             </Link>
-
+            <RecipeBadge :tags="tags" :is-name-displayed="false" />
         </div>
     </div>
 </template>
