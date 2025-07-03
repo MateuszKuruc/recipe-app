385712b97134deac9f71589b73e2957384f74569
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-03 22:14:29 +0200

    fix styling

--- resources/js/pages/categories/Show.vue
+++ resources/js/pages/categories/Show.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
+import Paginator from '@/components/Paginator.vue';
+import RecipeCard from '@/components/RecipeCard.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link } from '@inertiajs/vue3';
-import { Input } from '@/components/ui/input'
-import RecipeCard from '@/components/RecipeCard.vue';
-import ActionCard from '@/components/ActionCard.vue';
-import { Grid, Plus, Clock } from 'lucide-vue-next';
-import Paginator from '@/components/Paginator.vue';
-
+import { Head } from '@inertiajs/vue3';
 
 const props = defineProps({
     category: Object,
     recipes: Object,
-})
-
+});
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: props.category?.name || '',
         href: '/kategorie',
-    }
+    },
 ];
 </script>
 
@@ -28,16 +23,17 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-8 rounded-xl p-6">
-
             <div class="flex flex-col gap-y-4">
-                <h2 class="text-2xl font-bold border-b py-4">Wszystkie <span class="text-orange-500 lowercase">{{ category.name }}</span></h2>
-                <div class="relative min-h-[100vh] flex-1 grid grid-cols-2 gap-x-4 gap-y-4 rounded-xl border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <RecipeCard v-for="recipe in recipes.data" :key="recipe.id"  :recipe="recipe" :currentSlug="recipe.category.slug"  />
-
+                <h2 class="border-b border-gray-300 py-4 text-2xl font-bold">
+                    Wszystkie <span class="text-orange-500 lowercase">{{ category.name }}</span>
+                </h2>
+                <div
+                    class="relative grid min-h-[100vh] flex-1 grid-cols-2 gap-x-4 gap-y-4 rounded-xl border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
+                >
+                    <RecipeCard v-for="recipe in recipes.data" :key="recipe.id" :recipe="recipe" :currentSlug="recipe.category.slug" />
                 </div>
                 <Paginator :recipes="recipes" />
             </div>
-
         </div>
     </AppLayout>
 </template>
