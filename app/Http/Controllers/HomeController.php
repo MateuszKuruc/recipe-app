f6eb4efab0fc7e82fda39d60cdfae90ef4ea0357
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-07-04 00:43:45 +0200

    rewrite home controller to pass data needed to render homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,20 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Models\Recipe;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $user = auth()->user();
+        $favoriteRecipes = null;
+
+        if ($user && $user->favoriteRecipes()->count() >= 3) {
+            $favoriteRecipes = $user->favoriteRecipes()->with(['tags', 'category'])->inRandomOrder()->take(3)->get();
+        }
+
+        $randomRecipes = Recipe::with(['tags', 'category'])->inRandomOrder()->take(3)->get();
+
         $newestRecipes = Recipe::with(['tags', 'category'])
             ->latest()
             ->take(3)
             ->get();
 
+        $breakfastRecipes = Recipe::with(['tags', 'category'])
+            ->whereHas('category', fn($q) => $q->where('slug', 'sniadania'))
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
+
+        $dinnerRecipes = Recipe::with(['tags', 'category'])
+            ->whereHas('category', fn($q) => $q->where('slug', 'dania-glowne'))
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
+
+        $dessertRecipes = Recipe::with(['tags', 'category'])
+            ->whereHas('category', fn($q) => $q->where('slug', 'desery'))
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
+
+
         return inertia('Home', [
             'newestRecipes' => $newestRecipes,
+            'randomRecipes' => $randomRecipes,
+            'favoriteRecipes' => $favoriteRecipes,
+            'breakfastRecipes' => $breakfastRecipes,
+            'dinnerRecipes' => $dinnerRecipes,
+            'dessertRecipes' => $dessertRecipes,
         ]);
     }
 }
