70e89569da6bf493b75189081241475a7816de9f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-08-02 13:11:46 +0200

    add reusable component to display message when there is no data to show

---
+++ resources/js/pages/categories/NoResultsSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import LinkButton from '@/components/LinkButton.vue';
+const props = defineProps<{
+    displayData: boolean;
+    buttonLabel?: string;
+    buttonUrl?: string;
+}>();
+</script>
+
+<template>
+    <div v-if="displayData" class="mt-2 flex flex-col items-start gap-4">
+        <p class="text-xl">Brak przepisów w tej kategorii.</p>
+        <div class="flex gap-2">
+            <LinkButton :href="route('recipes.create')"> Dodaj nowy przepis</LinkButton>
+            <LinkButton v-if="buttonLabel && buttonUrl" :href="route('categories.index')" variant="outline"> {{ buttonLabel }}</LinkButton>
+        </div>
+    </div>
+</template>
